34489145ee0b83e6fed46521e198adfd1d0e61ed
Juliana Teles <jtelessoares.jt@gmail.com>
2022-10-14 21:16:51 -0300

    Adicionando algumas Implementações de apresentação a classe TesteCalculadoraCientifica

--- TesteCalculadoraCientifica.java
+++ TesteCalculadoraCientifica.java
@@ -2,14 +2,14 @@ public class TesteCalculadoraCientifica {
     public static void main(String[] args) {
         CalculadoraCientifica calculator = new CalculadoraCientifica();
 
-        byte a = 0, b = 1;
+        byte a = 3, b = -2;
 
-        calculator.raiz(2.0);
-        calculator.raiz("9");
-        calculator.raiz(100);
+        System.out.println(calculator.raiz(4.4));
+        System.out.println(calculator.raiz("4"));
+        System.out.println(calculator.raiz(100));
 
-        calculator.potencia("2", "2");
-        calculator.potencia(5, 8.9);
-        calculator.potencia(a,b);
+        System.out.println(calculator.potencia("2", "2"));
+        System.out.println(calculator.potencia(5, 0.5));
+        System.out.println(calculator.potencia(a,b));
     }
 }
